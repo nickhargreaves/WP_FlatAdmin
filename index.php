780e9fdc329f40ba1b35ace114df4fedf378347e
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-24 16:24:57 +0300

    add plugin info

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Flat UI Wordpress Admin Theme
+* Plugin URI: http://github.com/nickhargreaves/flat_ui_wp_admin
+* Description: This plugin turns a Wordpress Admin backend into a nice looking dashboard based on the Flat UI kit by http://designmodo.com/flat
+* Version: 1.0.0
+* Author: Nick Hargreaves
+* Author URI: http://nickhargreaves.com
+* License: GPL2
+*/
