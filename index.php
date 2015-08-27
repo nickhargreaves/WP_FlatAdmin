9c7cec9cb7def26ef735942cfb9caf5135dc118e
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-27 11:02:08 +0300

    we don't need custom class for menu items

--- index.php
+++ index.php
@@ -18,22 +18,4 @@ add_action('admin_enqueue_scripts', 'flat_admin_theme_style');
 add_action('login_enqueue_scripts', 'flat_admin_theme_style');
 
 
-//add custom class to admin menu
-add_action( 'admin_init','wpse_60168_custom_menu_class' );
-
-function wpse_60168_custom_menu_class()
-{
-    global $menu;
-
-    foreach( $menu as $key => $value )
-    {
-        //if(startsWith(($value[4]), "menu-top"))
-            //$menu[$key][4] .= " btn btn-block btn-lg btn-primary";
-    }
-
-}
-
-function startsWith($haystack, $needle) {
-    // search backwards starting from haystack length characters from the end
-    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
-}
+//
