83beb319abe050a1a2496a8c7939a1be9b3a3270
Nick Hargreaves <hargreaves1234@gmail.com>
2015-09-29 13:10:16 +0300

    custom url + title for login logo

--- login/config_options.php
+++ login/config_options.php
@@ -11,12 +11,25 @@ $opt_name = 'wp_flat_admin_custom_logo_path';
 $hidden_field_name = 'wp_flat_admin_submit_hidden';
 $data_field_name = 'wp_flat_admin_custom_logo_path';
 
+$custom_url_title = get_option( 'custom_url_title' );
+if(empty($custom_url_title)){
+    $custom_url_title = get_bloginfo('name');
+}
+
+
+$custom_url = get_option( 'custom_login_logo_url' );
+if(empty($custom_url)){
+    $custom_url = get_bloginfo('url');
+}
+
 //check for post
 if(isset($_POST[$data_field_name])){
     $opt_val = $_POST[ $data_field_name ];
 
     //Save the posted value in the database
     update_option( 'wp_flat_admin_custom_logo_path', $opt_val );
+    update_option( 'custom_login_logo_url', $_POST['custom_login_logo_url'] );
+    update_option( 'custom_url_title', $_POST['custom_url_title'] );
 }
 
 // Read in existing option value from database
@@ -41,17 +54,21 @@ echo "<h4>" . __( 'WP Flat Admin Settings', 'menu-test' ) . "</h4>";
    // wp_enqueue_media();
 ?>
 
-Upload image or add url:
-<br />
-<label for="upload_image">
-
-    <form action="" method="post" id="submit_picture">
-    <input id="upload_image" type="text" name="<?php echo $data_field_name; ?>"  value="<?php echo $opt_val; ?>" class="form-control"/>
+<form action="" method="post" id="submit_picture">
+    Custom login logo <br />
+    <input placeholder="Upload image or add url" id="upload_image" type="text" name="<?php echo $data_field_name; ?>"  value="<?php echo $opt_val; ?>" class="form-control"/>
     <input id="upload_image_button" class="button" type="button" value="Upload Custom Logo" />
     <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
-    <input type="submit" id="save_image" class="button button-primary" value="save"></form>
-
-</label>
+<br />
+    Custom login logo link
+    <br />
+    <input placeholder="Custom login logo link" type="text" name="custom_url"  value="<?php echo $custom_url; ?>" class="form-control"/>
+<br />
+    Custom login logo title<br />
+    <input placeholder="Custom login logo title" type="text" name="custom_url_title"  value="<?php echo $custom_url_title; ?>" class="form-control"/>
+<br />
+    <input type="submit" id="save_image" class="button button-primary" value="save">
+</form>
 <div id="show_image">
 
     <?php
--- login/login.php
+++ login/login.php
@@ -18,12 +18,21 @@ add_action( 'login_enqueue_scripts', 'custom_login_logo' );
 
 //custom logo url
 function custom_login_logo_url(){
-    return (get_bloginfo('url'));
+    $custom_url = get_option( 'custom_login_logo_url' );
+    if(empty($custom_url)){
+        $custom_url = get_bloginfo('url');
+    }
+    return ($custom_url);
 }
 add_filter('login_headerurl', 'custom_login_logo_url');
 
+//custom logo url title
 function custom_login_logo_url_title(){
-    return (get_bloginfo('name'));
+    $custom_url_title = get_option( 'custom_url_title' );
+    if(empty($custom_url_title)){
+        $custom_url_title = get_bloginfo('name');
+    }
+    return ($custom_url_title);
 }
 add_filter('login_headertitle',  'custom_login_logo_url_title');
 ?>
